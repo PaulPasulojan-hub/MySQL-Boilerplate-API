64f247565e262619183302e0c3a4336fa2cae670
PaulPasulojan <adrillepasulojan@gmail.com>
2026-05-27 01:37:53 +0800

    initial setup

---
+++ MySQL-Boilerplate-API/_helpers/db.ts
@@ -0,0 +1,11 @@
+﻿const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'id-dci-web1631.main-hosting.eu',
+  port: 3306,
+  user: 'u875409848_pasulojan',
+  password: '2qW^sUg=M',
+  database: 'u875409848_pasulojan'
+});
+
+export default db;
---
+++ MySQL-Boilerplate-API/app.ts
@@ -0,0 +1,24 @@
+﻿import express from 'express';
+import db from './_helpers/db';
+
+const config = require('./config.json');
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'API is running!' });
+});
+
+db.connect((err: any) => {
+  if (err) {
+    console.error('DB connection failed:', err.message);
+  } else {
+    console.log('Connected to MySQL!');
+    app.listen(config.server.port, () => {
+      console.log('Server running on port ' + config.server.port);
+    });
+  }
+});
+
+export default app;
