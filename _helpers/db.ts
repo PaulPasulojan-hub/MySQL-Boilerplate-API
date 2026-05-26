4a8c1fd7fdf5bef6de16889a9e1bc9acc6cb8b10
PaulPasulojan <adrillepasulojan@gmail.com>
2026-05-27 03:31:16 +0800

    fix db to use config

--- _helpers/db.ts
+++ _helpers/db.ts
@@ -1,27 +1,24 @@
-import config from '../config.json';
-import mysql from 'mysql2/promise';
-import { Sequelize } from 'sequelize';
-import accountModel from '../accounts/account.model';
-import refreshTokenModel from '../accounts/refresh-token.model';
+const mysql = require('mysql2');
+const config = require('../config.json');
 
-const db: any = {};
+const db = mysql.createConnection({
+  host: config.db.host,
+  port: config.db.port,
+  user: config.db.user,
+  password: config.db.password,
+  database: config.db.database
+});
+
+db.connect((err: any) => {
+  if (err) {
+    console.error('DB connection failed:', err.message);
+  } else {
+    console.log('Connected to MySQL!');
+  }
+});
+
+db.on('error', (err: any) => {
+  console.error('DB error:', err.message);
+});
+
 export default db;
-
-initialize();
-
-async function initialize() {
-    const { host, port, user, password, database } = config.database;
-    const connection = await mysql.createConnection({ host, port, user, password });
-
-    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
-
-    const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' }) ;
-
-    db.Account = accountModel(sequelize);
-    db.RefreshToken = refreshTokenModel(sequelize);
-
-    db.Account.hasMany(db.RefreshToken, { onDelete: 'CASCADE' });
-    db.RefreshToken.belongsTo(db.Account);
-
-    await sequelize.sync();
-}
