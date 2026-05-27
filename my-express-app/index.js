39b5a5f166d90495bd3f05862bc2b8ec136bab5c
PaulPasulojan <adrillepasulojan@gmail.com>
2026-05-27 22:31:30 +0800

    fix: switch to Sequelize, wire up auth routes

---
+++ my-express-app/index.js
@@ -0,0 +1,16 @@
+const express = require ('express');
+const app = express();
+const port = 3000;
+
+// Servce static files from the "public" folder
+app.use(express.static('public'));
+
+// Define a route for the home page
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log('Server running at http://localhost:${port}');
+});
