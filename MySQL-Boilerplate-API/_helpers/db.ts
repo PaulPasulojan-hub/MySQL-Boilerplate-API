bb945077427c4f63e8d07463f3aa38d7bddb3799
PaulPasulojan <adrillepasulojan@gmail.com>
2026-05-27 17:43:38 +0800

    fix: use connection pool to prevent ECONNRESET

--- MySQL-Boilerplate-API/_helpers/db.ts
+++ MySQL-Boilerplate-API/_helpers/db.ts
@@ -1,11 +1,26 @@
 const mysql = require('mysql2');
+const config = require('../config.json');
 
-const db = mysql.createConnection({
-  host: 'id-dci-web1631.main-hosting.eu',
-  port: 3306,
-  user: 'u875409848_pasulojan',
-  password: '2qW^sUg=M',
-  database: 'u875409848_pasulojan'
+const db = mysql.createPool({
+  host: config.db.host,
+  port: config.db.port,
+  user: config.db.user,
+  password: config.db.password,
+  database: config.db.database,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
+});
+
+db.getConnection((err: any, connection: any) => {
+  if (err) {
+    console.error('DB connection failed:', err.message);
+  } else {
+    console.log('Connected to MySQL!');
+    connection.release();
+  }
 });
 
 export default db;
