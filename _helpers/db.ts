0344eb2c42589e4f272c9782eb552a67039aa029
PaulPasulojan <adrillepasulojan@gmail.com>
2026-05-27 17:45:46 +0800

    fix: use connection pool

--- _helpers/db.ts
+++ _helpers/db.ts
@@ -1,24 +1,26 @@
 const mysql = require('mysql2');
 const config = require('../config.json');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: config.db.host,
   port: config.db.port,
   user: config.db.user,
   password: config.db.password,
-  database: config.db.database
+  database: config.db.database,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
 });
 
-db.connect((err: any) => {
+db.getConnection((err: any, connection: any) => {
   if (err) {
     console.error('DB connection failed:', err.message);
   } else {
     console.log('Connected to MySQL!');
+    connection.release();
   }
-});
-
-db.on('error', (err: any) => {
-  console.error('DB error:', err.message);
 });
 
 export default db;
